8f9cb1b0f0694b5b5dfdd81c2d0ccef6ae94c357
+ <+>
2025-05-06 19:25:23 +0300

    ipdbdebug

--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,20 @@
-#!/usr/bin/env python3
+# lib/ipdb_debugging.py
+import ipdb
 
-from ipdb_debugging import plus_two
+
+def plus_two(num):
+    ipdb.set_trace()
+    return num + 2
+ 
+
+# lib/testing/ipdb_debugging_test.py
+
+from lib.ipdb_debugging import plus_two
 
 class TestIpdbDebugging:
     '''ipdb_debugging.py'''
-    
+
     def test_adds_two(self):
         '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        assert plus_two(3) == 5
+    
